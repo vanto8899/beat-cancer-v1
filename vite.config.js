1ac8eebc3edda22fe83c9983b9353b2549148eb9
topv8899 <vanto8899@gmail.com>
2024-08-17 10:03:18 +0700

    fix code update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import path from "path";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react"; // Import the React plugin from Vite
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], // Use the React plugin to handle JSX and React transformations
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"), // Create an alias for easy imports from the src directory
     },
   },
-  base: "./",
+  base: "./", // This setting might be necessary if deploying to a non-root path
 });
